ce8d946c8d6feff1cbda2400b40c1374c9c1eeb3
MarioStation <lartu@seznam.cz>
2023-02-27 22:16:46 +0100

    make it work at home:

--- resources/views/admin/authors.blade.php
+++ resources/views/admin/authors.blade.php
@@ -5,6 +5,7 @@
         <div class="author">
             <div class="author-id">{{ $author->id }}</div>
             <div class="author-name">{{ $author->name }}</div>
+            <div class="author-bio">{!! $author->bio !!}</div>
         </div>
     @endforeach
 @endsection
